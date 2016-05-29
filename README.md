7f1ab9c69e7e2d1ad9b7438dd29bfa9940d86f9f
Eduardo Garcia <egarcia@ooyala.com>
2016-05-29 16:11:33 -0500

    Add GET by id, POST, PUT, DELETE support for /publishing and /reach

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Future work
 
 + Use a proper database, not in-memory storage
++ Restructure Reach API to have only one timestamp per object
++ Add `id` field to reach elements to be able to read/update/delete in a better way (as opposed to the current
+  index implementation), as well as keep DELETE operations idempotent
++ Add PATCH support for partial updates
++ Add testing to both backend and frontend
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const app = require('express')(),
     http = require('http').Server(app),
+    json = require('body-parser').json(),
     session = require('express-session'),
     settings = require('./settings.json'),
     fixtures = {
@@ -22,6 +23,7 @@ let Server = {
       secret: 'socialfalcon'
     }));
 
+    app.use(json);
     app.use(this.loadFixtures);
 
     this.addRestEndpoints();
@@ -32,12 +34,96 @@ let Server = {
   },
 
   addRestEndpoints() {
+    // Publishing
     app.get('/publishing', function(request, response) {
       response.json(request.session.publishing);
     });
 
+    app.get('/publishing/:id', function(request, response) {
+      let publishing = request.session.publishing.find((element) => {
+        return element.id === request.params.id;
+      });
+
+      if(publishing) {
+        response.json(publishing);
+      } else {
+        response.status(404).send('Not found');
+      }
+    });
+
+    app.post('/publishing', function(request, response) {
+      request.session.publishing.push(request.body);
+
+      response.status(200).send('OK');
+    });
+
+    app.put('/publishing/:id', function(request, response) {
+      let index = request.session.publishing.findIndex((element) => {
+        return element.id === request.params.id;
+      });
+
+      if(index === -1) {
+        response.status(404).send('Not found');
+      } else {
+        request.session.publishing[index] = request.body;
+
+        response.status(200).send('OK');
+      }
+    });
+
+    app.delete('/publishing/:id', function(request, response) {
+      let index = request.session.publishing.findIndex((element) => {
+        return element.id === request.params.id;
+      });
+
+      if(index === -1) {
+        response.status(404).send('Not found');
+      } else {
+        request.session.publishing.splice(index, 1);
+
+        response.status(200).send('OK');
+      }
+    });
+
+    // Reach
     app.get('/reach', function(request, response) {
       response.json(request.session.reach);
+    });
+
+    app.get('/reach/:index', function(request, response) {
+      let reach = request.session.reach[request.params.index];
+
+      if(reach) {
+        response.json(reach);
+      } else {
+        response.status(404).send('Not found');
+      }
+    });
+
+    app.post('/reach', function(request, response) {
+      request.session.reach.push(request.body);
+
+      response.status(200).send('OK');
+    });
+
+    app.put('/reach/:index', function(request, response) {
+      if(request.session.reach[request.params.index]) {
+        request.session.reach[request.params.index] = request.body;
+
+        response.status(200).send('OK');
+      } else {
+        response.status(404).send('Not found');
+      }
+    });
+
+    app.delete('/reach/:index', function(request, response) {
+      if(request.session.reach[request.params.index]) {
+        request.session.reach.splice(request.params.index, 1);
+
+        response.status(200).send('OK');
+      } else {
+        response.status(404).send('Not found');
+      }
     });
   },
 
